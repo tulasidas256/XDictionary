05ea6218795b54af1fe2540d77436e450884a222
Tulasidas Nayak <tulasidasnayak68@gmail.com>
2024-02-28 22:51:23 +0530

    XDictionary

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+
+const dictionary = [
+
+  { word: "React", meaning: "A JavaScript library for building user interfaces." },
+
+  { word: "Component", meaning: "A reusable building block in React." },
+
+  { word: "State", meaning: "An object that stores data for a component." }
+
+]
+
+
+function App() {
+
+  const [inputText, setinputText] = useState("");
+  const [result, setResult] = useState("");
+  //const [show, setShow] = useState(false);
+
+  const handleChange = (e) => {
+    let text = e.target.value.toLowerCase();
+    setinputText(text);
+  };
+
+  const handleSearch = () => {
+    let word = inputText.toLocaleLowerCase();
+    let correctInput = dictionary.filter((itm) =>
+      itm.word.toLocaleLowerCase() === word
+    );
+    console.log('<correctInput>',correctInput)
+
+    if (correctInput.length === 0) {
+      setResult("Word not found in the dictionary.");
+    } else {
+      console.log('Defination =>',correctInput[0].meaning);
+      setResult(correctInput[0].meaning);
+    }
+  };
+
+
+  useEffect(() => {
+  }, []);
+
+  useEffect(() => {
+    console.log("render");
+  }, [result]);
+
+
+
+  return (
+    <div>
+      <h1>Dictionary App</h1>
+      <input type='text'
+        placeholder='Search for a word...'
+        onChange={handleChange}
+        required />
+      <button onClick={handleSearch}>Search</button>
+      <br />
+      <span className='def'>Defination:</span>
+      <div>
+        <p>{result}</p>
+      </div>
+    </div>
+  );
+
+}
+
+export default App;
